815951edefa6673c5ca8c1f2335ae45585f7dcfc
Mountaser164 <9015678@student.zadkine.nl>
2024-06-06 14:02:17 +0200

    email

--- back/constants.php
+++ back/constants.php
@@ -1,5 +1,5 @@
 <?php
-    $admin_email = "rubaawata97@gmail.com";
-    $admin_phone = "01008434175";
-    $admin_whatsapp = "+201008434175";
+    $admin_email = "mmwnty835@gmail.com";
+    $admin_phone = "0684145028";
+    $admin_whatsapp = "+31684145028";
 ?>
